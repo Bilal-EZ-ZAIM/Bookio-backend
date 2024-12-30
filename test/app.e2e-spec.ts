2105483579299fb7a9111544d65cda30a7b8db38
Bilal-EZ-ZAIM <bilalzail499@gmail.com>
2024-12-30 11:17:40 +0100

    Ajout de la validation JWT pour sécuriser les routes API.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -4,7 +4,8 @@ import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
+  let app: INestApplication;  
+  //SSS
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
